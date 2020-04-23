ff5d93e0852ac2f0e48d4f5a72cb2cf9062a4f4a
Zack Davenport <zackdavenport@tcadmins-Air.broadband>
2020-04-23 16:14:08 +0100

    changes made and reverse proxy working

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -23,34 +23,7 @@ sudo apt-get install nginx -y
 
 # remove the old file and add our one
 sudo rm /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/environment/reverse-proxy.conf /etc/nginx/sites-available/default
 
 # finally, restart the nginx service so the new config takes hold
-sudo service nginx restart
-
-sudo unlink /etc/nginx/sites-enabled/default
-
-cd /etc/nginx/sites-available
-sudo touch reverse-proxy.conf
-
-reverse-proxy.conf << EOF
-server {
-        listen 80;
-        listen [::]:80;
-
-        access_log /var/log/nginx/reverse-access.log;
-        error_log /var/log/nginx/reverse-error.log;
-
-        location / {
-                    proxy_pass http://192.168.10.100:3000;
-  }
-}
-EOF
-
-sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
-sudo service nginx configtest
-sudo service nginx restart
-sleep 10
-sudo service nginx restart
-cd /home/ubuntu/app
-nodejs app.js
+sudo systemctl restart nginx
